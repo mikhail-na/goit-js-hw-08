af51cee478b94acb7535e829682d0e0ffc78f817
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-04-03 01:41:43 +0200

    added new tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
+//* Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+//? Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm(посилання на CDN 
+// з твоєї минулої роботи більше не потрібне).
+
+//? Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з урахуванням того,
+// що бібліотека була встановлена через npm(синтаксис import /export).
+        
+//? Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в 
+// документації.
+
+
+
+
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryListEl = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', markup);
+
+// rendered items
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form ');
+const KEY = 'feedback-form-state';
+
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(KEY, JSON.stringify(dataForm));
+};
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert("Будь ласка, заповніть УСІ обов'язкові поля.");
+  }
+
+  localStorage.removeItem(KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+};
